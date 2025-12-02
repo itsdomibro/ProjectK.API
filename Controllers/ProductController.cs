ec6d9b0eaa542ca163baa4980d91ca31b9ef9904
Jefferson <jefferson.swanto@gmail.com>
2025-12-02 21:23:51 +0700

    feat: product controller and dto

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectK.API.Data;
+using ProjectK.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using ProjectK.API.DTOs;
+
+
+namespace ProjectK.API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetProduct(
+            [FromQuery] string? search,
+            [FromQuery] Guid? categoryId)
+        {
+            
+            var ownerId = GetCurrentOwnerId();
+
+            var query = _context.Products
+            .Where(p => p.UserId == ownerId)
+            .AsQueryable();
+
+            if (categoryId.HasValue)
+            {
+                query = query.Where(p => p.CategoryId == categoryId);
+            }
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                search = search.ToLower();
+                query = query.Where(p =>
+                    p.Name.ToLower().Contains(search) ||
+                    (p.Description != null && p.Description.ToLower().Contains(search))
+                );
+            }
+
+
+            // user id di product  = owner id
+            var products = await query.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Owner")]
+        public async Task<IActionResult> CreateProduct(CreateProductDto dto)
+        {
+            var ownerId = GetCurrentOwnerId();
+
+            var product = new Product
+            {
+                UserId = ownerId,
+                ProductId = Guid.NewGuid(),
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price,
+                Discount = dto.Discount,
+                CategoryId = dto.CategoryId,
+                ImageUrl = dto.ImageUrl,
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+
+        }
+
+        private Guid GetCurrentOwnerId()
+        {
+            var role = User.FindFirst(ClaimTypes.Role)?.Value;
+            var userId = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            Console.WriteLine(userId +  role);
+
+            if (string.IsNullOrEmpty(role) || string.IsNullOrEmpty(userId))
+                throw new UnauthorizedAccessException("Invalid token.");
+
+            var currentUserId = Guid.Parse(userId);
+
+            var user = _context.Users.FirstOrDefault(u => u.UserId == currentUserId);
+
+            if(user == null)
+                throw new UnauthorizedAccessException("User not found.");   
+
+            if (role == "Owner")
+            {
+                return user.UserId;
+            }
+
+            if (role == "Cashier")
+            {
+                return user.OwnerId ?? Guid.Empty;
+            }
+
+            throw new UnauthorizedAccessException("Role not supported");
+        } 
+    }
+}
---
+++ DTOs/CreateProductDto.cs
@@ -0,0 +1,14 @@
+﻿using ProjectK.API.Models;
+
+namespace ProjectK.API.DTOs
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public Guid? CategoryId { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
