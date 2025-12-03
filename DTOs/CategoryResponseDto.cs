d0071150dad77c003e5dbf37373f9aa958bd1efc
Jefferson <jefferson.swanto@gmail.com>
2025-12-03 21:11:25 +0700

    feat: add category controller (CRUD logic)

---
+++ DTOs/CategoryResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectK.API.DTOs
+{
+    public class CategoryResponseDto
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+    }
+}
---
+++ DTOs/CreateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectK.API.DTOs
+{
+    public class CreateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
