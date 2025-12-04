0d0c1f5dd634dfb6a5e3a4f53dd422d4cde5fdc9
Jefferson <jefferson.swanto@gmail.com>
2025-12-04 17:43:29 +0700

    feat: add post method for transaction

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectK.API.Data;
+using System.Security.Claims;
+using ProjectK.API.DTOs.TransactionDto;
+using ProjectK.API.Models;
+
 
 namespace ProjectK.API.Controllers
 {
     [Route("api/transactions")]
     [ApiController]
+    [Authorize]
     public class TransactionsController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -18,9 +25,69 @@ namespace ProjectK.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTransactionDto dto)
+        {
+            var ownerId = await GetCurrentOwnerIdAsync();
+            if(dto.Items == null  || !dto.Items.Any())
+            {
+                return BadRequest("Transaction must have at least one item.");
+            }
 
+            var productIds = dto.Items
+                .Select(i => i.ProductId)
+                .Distinct()
+                .ToList();
 
-            return Ok();
+            var validProductIds = await _context.Products
+                .Where(p => p.UserId == ownerId && productIds.Contains(p.ProductId))
+                .Select(p => p.ProductId)
+                .ToListAsync();
+
+            if (validProductIds.Count != productIds.Count)
+            {
+                return BadRequest("One or more product IDs are invalid or not owned by this user.");
+            }
+
+            var transaction = new Transaction
+            {
+                TransactionId = Guid.NewGuid(),
+                UserId = ownerId,
+                Payment = dto.Payment,
+                Code = "SKADKAW", // Generate code logic here
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = null,
+                IsPaid = false,
+                IsDeleted = false
+            };
+
+            _context.Transactions.Add(transaction);
+            
+            foreach (var item in dto.Items)
+            {
+                var transactionDetail = new TransactionDetail
+                {
+                    TransactionDetailId = Guid.NewGuid(),
+                    TransactionId = transaction.TransactionId,
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity,
+                    CreatedAt = DateTime.UtcNow,
+                };
+                _context.TransactionDetails.Add(transactionDetail);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new TransactionResponseDto
+            {
+                TransactionId = transaction.TransactionId,
+                IsPaid = transaction.IsPaid,
+                PaymentName = transaction.Payment,
+                Code = transaction.Code,
+                Timestamp = transaction.CreatedAt
+            });
         }
 
         [HttpGet("{id}")]
@@ -29,16 +96,39 @@ namespace ProjectK.API.Controllers
             return Ok();
         }
 
-        [HttpPatch("{id}")]
-        public async Task<IActionResult> Edit()
-        {
-            return Ok();
-        }
-
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete()
         {
             return Ok();
+        }
+
+        private async Task<Guid> GetCurrentOwnerIdAsync()
+        {
+
+            var role = User.FindFirst(ClaimTypes.Role)?.Value;
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(role) || string.IsNullOrEmpty(userId))
+                throw new UnauthorizedAccessException("Invalid token.");
+
+            var currentUserId = Guid.Parse(userId);
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == currentUserId);
+
+            if (user == null)
+                throw new UnauthorizedAccessException("User not found.");
+
+            if (role == "Owner")
+            {
+                return user.UserId;
+            }
+
+            if (role == "Cashier")
+            {
+                return user.OwnerId ?? Guid.Empty;
+            }
+
+            throw new UnauthorizedAccessException("Role not supported");
         }
     }
 }
